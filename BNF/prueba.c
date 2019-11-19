1d77d35ac54d21f5c22ef2be68ba42bea123c61c
kevinoskar <kevin.oskar7@gmail.com>
2019-11-18 21:47:24 -0600

    Carpeta BNF

---
+++ BNF/prueba.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <math.h>
+
+int main(void) {
+
+    int valorUsuario;
+    printf("Introduzca un numero entero: ");
+    scanf("%d", &valorUsuario);
+    valorUsuario=valorUsuario+2;
+    printf("EL Resultado es: %d\n",valorUsuario);
+    printf("Gracias");
+    return 0;
+
+    
+}
