5e0575f54586596877dc9eb0d946687fda2124eb
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2019-12-01 02:16:44 -0600

    analizador Lexico 5%

--- Analizador Lexico en C/analizador_lex_En_C.c
+++ Analizador Lexico en C/analizador_lex_En_C.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
+#define MAX 100
 int main(void){
-    int entero=0;
-    double decimal=0;
-    char cadena=0;
+    
+    //Declaracioness
+    char buf[MAX];
+    char *ptr=buf;
     char palabras_reservadas[][11]={"definir","hasta que","mientras","repetir","si","hacer","finsi","sino","leer","imprimir","cursor"};
 
-    printf("--------------ANALIZADOR LEXICO EN C--------------");
-    printf("Ingrese un valor en la cadena ");
-    scanf(entero);
-    for (int i = 0; i < 11; i++){
-       printf("En el arreglo tienes el valor %s \n",palabras_reservadas[i]);
-    }
 
+    //Estados o Patrones
+    printf("--------------ANALIZADOR LEXICO EN C-------------- \n");
+    gets(buf);
+    if(*ptr =='\0'){
+        printf("Error, no se ingreso ningun caracter",stderr);
+        exit(1);
+    }
+    
+    while (*ptr != '\0'){
+        printf("%c",*ptr);
+
+
+        //Case Maquina de Estados
+        switch (expression)
+        {
+        case /* constant-expression */:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+        *ptr++;
+    }
 
 }
