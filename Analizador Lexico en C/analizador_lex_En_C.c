2add57fe1b4677aaf99997837ab9b458dfb4aa87
kevinoskar <47488256+kevinoskar@users.noreply.github.com>
2019-12-01 15:32:05 -0600

    15:31 dAnalizador lex c

--- Analizador Lexico en C/analizador_lex_En_C.c
+++ Analizador Lexico en C/analizador_lex_En_C.c
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 
 #define MAX 100
+
+
+
+
 int main(void){
     
     //Declaracioness
-    char buf[MAX];
+    char buf[MAX],caracter;
     char *ptr=buf;
     char palabras_reservadas[][11]={"definir","hasta que","mientras","repetir","si","hacer","finsi","sino","leer","imprimir","cursor"};
-
-
+    char letras_minusculas[][25]={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    int Estado=0;   //Estado Iniciak en 0
+    
+    
     //Estados o Patrones
     printf("--------------ANALIZADOR LEXICO EN C-------------- \n");
     gets(buf);
@@ -19,20 +25,67 @@ int main(void){
     }
     
     while (*ptr != '\0'){
-        printf("%c",*ptr);
-
-
+        //printf("%c",*ptr);
+        caracter=*ptr;
         //Case Maquina de Estados
-        switch (expression)
+        for (int i = 0; i < (sizeof(letras_minusculas)/sizeof(letras_minusculas[0])); i++)
         {
-        case /* constant-expression */:
-            /* code */
+            if (caracter == letras_minusculas[i])
+            {
+                printf("Encontre la letra %s %c \n",letras_minusculas[i],caracter);
+            }
+
+        }
+        
+
+
+        switch (caracter){
+        case '0':
+           printf("Se ha encontrado un numero: %i \n",0);
+            break;
+        case '1':
+           printf("Se ha encontrado un numero: %i \n",1);
+            break;
+        case '2':
+           printf("Se ha encontrado un numero: %i \n",2);
+            break;
+        case '3':
+           printf("Se ha encontrado un numero: %i \n",3);
+            break;
+        case '4':
+           printf("Se ha encontrado un numero: %i \n",4);
+            break;
+        case '5':
+           printf("Se ha encontrado un numero: %i \n",5);
+            break;
+        case '6':
+           printf("Se ha encontrado un numero: %i \n",6);
+            break;
+        case '7':
+           printf("Se ha encontrado un numero: %i \n",7);
+            break;
+        case '8':
+           printf("Se ha encontrado un numero: %i \n",8);
+            break;
+        case '9':
+           printf("Se ha encontrado un numero: %i \n ",9);
             break;
         
+
+
+
+
+
+
+
+
+
+
         default:
+           // printf("Se ha encontrado una letra: %c \n ",caracter);
             break;
         }
         *ptr++;
     }
-
+    
 }
